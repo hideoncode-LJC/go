11430ec730d63711f46d62c3e6c96e619ce9354e
hideoncoder <hideonlife@163.com>
2023-09-17 15:55:03 +0800

    5 commit

--- go-standard/context/context-example-2/main.go
+++ go-standard/context/context-example-2/main.go
@@ -1,25 +1,50 @@
 package main
 
-type singal struct{}
+import (
+	"log"
+	"math/rand"
+	"time"
+)
 
-func doTask(i int, s singal) {
+type Signal chan struct{}
+
+/*
+轮询定时查询
+*/
+
+func doTask(s Signal) {
+	t := 0
+
 	for {
 		select {
-		case <- s :
-			fmt.Println("Task ", i, "Done !")
+		case <-s:
+			log.Println("Task Done !")
+			break
 		default:
-			fmt.Println("Task ", i, "Go on!")
+			log.Println("Task Go on!")
 		}
+		time.Sleep(time.Second)
+		log.Printf("Task 已经运行 %d 秒", t)
+		t++
 	}
 }
 
 func main() {
-	stop := singal{}
-	for i := 0 ; i < 3 ; i++ {
-		go doTask(i, stop)
-	}
-	time.Sleep(time.)
+
+	s := make(Signal)
+
+	go doTask(s)
+
 	for {
+		n := rand.Int()
+
+		if n%2 == 0 {
+			s <- struct{}{}
+			time.Sleep(3 * time.Second)
+			break
+		}
+
+		time.Sleep(time.Second)
 
 	}
 }
